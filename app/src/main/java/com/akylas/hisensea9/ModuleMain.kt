27ec743e284123e656540b0b9047852aa5f1611a
Martin Guillon <dev@akylas.fr>
2025-05-14 18:52:40 +0200

    chore: open camera on longpress eink button

--- app/src/main/java/com/akylas/hisensea9/ModuleMain.kt
+++ app/src/main/java/com/akylas/hisensea9/ModuleMain.kt
@@ -239,7 +239,7 @@ class ModuleMain : IXposedHookLoadPackage {
                   // Handle long press
                   val prefs = Preferences()
                   if (pref.getBoolean("eink_longpress_camera", false)) {
-                      val intent = Intent(MediaStore.INTENT_ACTION_STILL_IMAGE_CAMERA).apply {
+                      val intent = Intent(MediaStore.INTENT_ACTION_STILL_IMAGE_CAMERA_SECURE).apply {
                         addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
                       }
                       try {
