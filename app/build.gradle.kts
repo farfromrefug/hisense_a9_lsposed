fa12fd448cd080422766b46479915be49a80a4bc
Martin Guillon <dev@akylas.fr>
2026-08-14 23:10:48 +0000

    chore: new build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
     defaultConfig {
         minSdk = 24
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.0.2"
+        versionCode = 4
+        versionName = "1.0.3"
     }
     buildFeatures {
         buildConfig = true
